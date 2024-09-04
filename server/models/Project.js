abea41448a353d532eb0936bb0dff4d7b64f602d
kms9320 <kimminseo9320@gmail.com>
2024-09-05 00:33:27 +0900

    Initial commit

---
+++ server/models/Project.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  link: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
